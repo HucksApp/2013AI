a978781edf0eaaceb5edb1ace74c61f13b3331e5
Wu Zhongxian <concisense@gmail.com>
2014-01-12 03:13:32 +0800

    Refactor the evaluation function into class

--- testStructure/hungryAgents.py
+++ testStructure/hungryAgents.py
@@ -25,7 +25,7 @@ class HungryBomberman(Agent):
     if len(successors) is 0: return Directions.STOP
     if len(successors) is 1: return successors[0][1]
     scored = [(
-        hungryEvaluation(nstate, _currpos, self.index, False), action
+        self.hungryEvaluation(nstate, _currpos, self.index, False), action
         ) for nstate, action in successors]
     ###print 'scored=',scored,
     bestScore = max(scored)[0]
@@ -35,108 +35,105 @@ class HungryBomberman(Agent):
     print
     return ret
 
-def hungryEvaluation(nstate, oldpos, agentIdx, hasBombLeft=False):
-  """
-  NOTE: Currently, does not consider enemy's distance to the items
-  """
-  ox, oy = oldpos
-  nx, ny = nstate.getAgentPosition(agentIdx)
-  dx, dy = nx - ox, ny - oy
+  def hungryEvaluation(self, nstate, oldpos, agentIdx, hasBombLeft=False):
+    """
+    NOTE: Currently, does not consider enemy's distance to the items
+    """
+    ox, oy = oldpos
+    nx, ny = nstate.getAgentPosition(agentIdx)
+    dx, dy = nx - ox, ny - oy
 
-  if dx == 0:
-    tx = int(ox)
-  else:
-    tx = (int(math.ceil(nx)) if dx > 0 else int(math.floor(nx)))
+    if dx == 0:
+      tx = int(ox)
+    else:
+      tx = (int(math.ceil(nx)) if dx > 0 else int(math.floor(nx)))
 
-  if dy == 0:
-    ty = int(oy)
-  else:
-    ty = (int(math.ceil(ny)) if dy > 0 else int(math.floor(ny)))
+    if dy == 0:
+      ty = int(oy)
+    else:
+      ty = (int(math.ceil(ny)) if dy > 0 else int(math.floor(ny)))
 
-  # Items are 1 ~ 9
-  #   1 - A - Item add Power
-	#   2 - S - Item add Speed
-	#   3 - N - Item add Bomb_Number
-  # Which do I prefer?
-  #
-  # Here are functions caculating satisfaction degree about each item
-  caculator = {
-      1: lambda speed: (speed + 1) / 5,
-      2: lambda power: (power + 1) / 8,
-      3: lambda nbomb: (nbomb    ) / 10 }
-  agentState = nstate.getAgentState(agentIdx)
-  state = {
-      1: agentState.speed,              # speed 0 ~ 4
-      2: agentState.Bomb_Power,         # power 0 ~ 7
-      3: agentState.Bomb_Total_Number } # nbomb 0 ~ 10
-  satisfaction_item_pairs = [(caculator[i](state[i]), i) for i in state]
-  satisfaction_order = [item_id for _, item_id in sorted(satisfaction_item_pairs)]
+    # Items are 1 ~ 9
+    #   1 - A - Item add Power
+    #   2 - S - Item add Speed
+    #   3 - N - Item add Bomb_Number
+    # Which do I prefer?
+    #
+    # Here are functions caculating satisfaction degree about each item
+    caculator = {
+        1: lambda speed: (speed + 1) / 5,
+        2: lambda power: (power + 1) / 8,
+        3: lambda nbomb: (nbomb    ) / 10 }
+    agentState = nstate.getAgentState(agentIdx)
+    state = {
+        1: agentState.speed,              # speed 0 ~ 4
+        2: agentState.Bomb_Power,         # power 0 ~ 7
+        3: agentState.Bomb_Total_Number } # nbomb 0 ~ 10
+    satisfaction_item_pairs = [(caculator[i](state[i]), i) for i in state]
+    satisfaction_order = [item_id for _, item_id in sorted(satisfaction_item_pairs)]
 
 
-  distances_to_item = { 1: [], 2: [], 3: [] }
-  # Perform BFS starts from pos=(tx, ty) within 20 steps
-  # to find all reachable items (if exists)
-  # and fill their distances into distances_to_item dict
-  currmap = nstate.data.map
-  startpos = (tx, ty)
+    distances_to_item = { 1: [], 2: [], 3: [] }
+    # Perform BFS starts from pos=(tx, ty) within 20 steps
+    # to find all reachable items (if exists)
+    # and fill their distances into distances_to_item dict
+    currmap = nstate.data.map
+    startpos = (tx, ty)
 
-  queue = deque()
-  visited = set()
-  adjacent_positions = lambda x, y: ((x-1, y), (x+1, y), (x, y-1), (x, y+1))
+    queue = deque()
+    visited = set()
+    adjacent_positions = lambda x, y: ((x-1, y), (x+1, y), (x, y-1), (x, y+1))
 
-  queue.append((startpos, 0))
-  visited.add(startpos)
-  ###print 'Start BFS',
-  while len(queue) != 0:
-    (x, y), this_dist = queue.popleft()
-    if this_dist > 20: break
-    item_id = currmap[x][y]
-    if item_id in distances_to_item:
-      distances_to_item[item_id].append(this_dist)
-      ###print '(%d, %d)' % (x, y),
-    for adjpos in adjacent_positions(x, y):
-      if (not currmap.isBlocked(adjpos) and
-          adjpos[0] in range(currmap.width) and
-          adjpos[1] in range(currmap.height) and
-          adjpos not in visited):
-        queue.append((adjpos, this_dist + 1))
-        visited.add(adjpos)
-  ###print 'End BFS'
+    queue.append((startpos, 0))
+    visited.add(startpos)
+    ###print 'Start BFS',
+    while len(queue) != 0:
+      (x, y), this_dist = queue.popleft()
+      if this_dist > 20: break
+      item_id = currmap[x][y]
+      if item_id in distances_to_item:
+        distances_to_item[item_id].append(this_dist)
+        ###print '(%d, %d)' % (x, y),
+      for adjpos in adjacent_positions(x, y):
+        if (not currmap.isBlocked(adjpos) and
+            adjpos[0] in range(currmap.width) and
+            adjpos[1] in range(currmap.height) and
+            adjpos not in visited):
+          queue.append((adjpos, this_dist + 1))
+          visited.add(adjpos)
+    ###print 'End BFS'
 
-  # Now we have some stuff like these
-  #   satisfaction_order = [2, 1, 3]
-  #   distances_to_item = { 1: [3, 5], 2: [1], 3: [2, 8] }
-  #
-  # For this state, how to score?  Sum all items up
-  #   Distence  0   1   2   3   4 ... 19  >=20
-  #   Score     20  19  18  17  16    1   0
-  #   (Three items: The most wanted x1, the 2nd x0.8, the 3rd x0.5)
-  score = 0
-  weight = [1, 1, 1]
-  for item_id in distances_to_item:
-    w = weight[satisfaction_order.index(item_id)]
-    for _dist in distances_to_item[item_id]:
-      if _dist == 0:
-        score += 100
-      else:
-        score += w * (20 - _dist)
-  ###print 'distances_to_item=', distances_to_item
+    # Now we have some stuff like these
+    #   satisfaction_order = [2, 1, 3]
+    #   distances_to_item = { 1: [3, 5], 2: [1], 3: [2, 8] }
+    #
+    # For this state, how to score?  Sum all items up
+    #   Distence  0   1   2   3   4 ... 19  >=20
+    #   Score     20  19  18  17  16    1   0
+    #   (Three items: The most wanted x1, the 2nd x0.8, the 3rd x0.5)
+    score = 0
+    weight = [1, 1, 1]
+    for item_id in distances_to_item:
+      w = weight[satisfaction_order.index(item_id)]
+      for _dist in distances_to_item[item_id]:
+        if _dist == 0:
+          score += 100
+        else:
+          score += w * (20 - _dist)
+    ###print 'distances_to_item=', distances_to_item
 
-  overall_ability = sum(state[i] for i in state) # compensation after eating item...
-  ret = score + overall_ability * 1000
-  ###print '(ox,oy)=(%1.1f,%1.1f) (nx,ny)=(%1.1f,%1.1f) (tx,ty)=(%d,%d)'%(ox,oy,nx,ny,tx,ty),
-  ###print ' has score=%d'%ret
-  return ret
+    overall_ability = sum(state[i] for i in state) # compensation after eating item...
+    ret = score + overall_ability * 1000
+    ###print '(ox,oy)=(%1.1f,%1.1f) (nx,ny)=(%1.1f,%1.1f) (tx,ty)=(%d,%d)'%(ox,oy,nx,ny,tx,ty),
+    ###print ' has score=%d'%ret
+    return ret
 
-class PutBomberman(Agent):
+class HungryPutBomb(Agent):
 
   def __init__(self, index=0, *_, **__):
     self.index = index
 
   def getAction(self, state):
-    """
-    Return an action
-    """
     if self.isHungryPutBombConditionHolds():
       if self.targetPutBombPosition != None:
         # 嘗試去接近那個位置
